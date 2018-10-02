9bf2192317b2f7b2adfcadad8cae4a2e7c5c06d2
Emese Vass <vassmse@gmail.com>
2018-10-02 10:10:14 +0200

    Kanban5 ready

--- AgendaPL/Views/KanbanExtendedPage.xaml.cs
+++ AgendaPL/Views/KanbanExtendedPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using AgendaCON.Models;
+using AgendaPL.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +25,97 @@ namespace AgendaPL.Views
     /// </summary>
     public sealed partial class KanbanExtendedPage : Page
     {
+        ViewModelLocator vm = new ViewModelLocator();
+        public MainViewModel ViewModel { get; set; }
+
         public KanbanExtendedPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            ViewModel = vm.MainPage;
+            DataContext = ViewModel;
+        }
+
+        private async void Grid_Drop(object sender, DragEventArgs e)
+        {
+            if (((e.OriginalSource as ScrollViewer)?.DataContext is MainViewModel targetAccount) && (e.OriginalSource as ScrollViewer).Name != null)
+                if (await (e.DataView.GetDataAsync("ID")) is int taskId)
+                {
+                    try
+                    {
+                        var targetState = (e.OriginalSource as ScrollViewer)?.Name;
+                        switch (targetState)
+                        {
+                            case "BacklogPanel":
+                                ViewModel.ChangeTaskState(taskId, 0);
+                                break;
+                            case "ToDoPanel":
+                                ViewModel.ChangeTaskState(taskId, 1);
+                                break;
+                            case "InProgressPanel":
+                                ViewModel.ChangeTaskState(taskId, 2);
+                                break;
+                            case "TestingPanel":
+                                ViewModel.ChangeTaskState(taskId, 3);
+                                break;
+                            case "DonePanel":
+                                ViewModel.ChangeTaskState(taskId, 4);
+                                break;
+                        }
+                    }
+                    catch (Exception ex) { Console.WriteLine(ex); }
+                }
+        }
+
+        private void Grid_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Move;
+            e.DragUIOverride.IsGlyphVisible = true;
+            e.DragUIOverride.IsContentVisible = true;
+            e.DragUIOverride.IsCaptionVisible = true;
+        }
+
+        private void TextBlock_DragStarting(UIElement sender, DragStartingEventArgs args)
+        {
+            if ((sender as StackPanel)?.DataContext is TaskDto task)
+            {
+                args.AllowedOperations = DataPackageOperation.Move;
+                args.Data.SetData("ID", task.TaskId);
+            }
+        }
+
+        private void ItemsControl_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            Window.Current.CoreWindow.PointerCursor = new Windows.UI.Core.CoreCursor(Windows.UI.Core.CoreCursorType.Hand, 0);
+
+        }
+
+        private void ItemsControl_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            Window.Current.CoreWindow.PointerCursor = new Windows.UI.Core.CoreCursor(Windows.UI.Core.CoreCursorType.Arrow, 0);
+
+        }
+
+
+        private void AddNewTask(object sender, PointerRoutedEventArgs e)
+        {
+            ViewModel.AddNewTask();
+        }
+
+        private void doneControl_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            TaskDto selectedTask;
+            if (e.OriginalSource as TextBlock == null)
+            {
+                var source = e.OriginalSource as StackPanel;
+                selectedTask = source.DataContext as TaskDto;
+            }
+            else
+            {
+                var source = e.OriginalSource as TextBlock;
+                selectedTask = source.DataContext as TaskDto;
+            }
+
+            ViewModel.SelectedTaskAction(selectedTask);
         }
     }
 }
